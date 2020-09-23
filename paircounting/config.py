fb9104ad487972f74dada2181904ee5feeb9a658
Cameron Grove <cameron.grove@durham.ac.uk>
2020-09-23 15:20:36 +0100

    Added config file, fixed small bugs in fasthod.py Added python scripts for different combinations of central and satellite pairs

---
+++ paircounting/config.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+path = "/cosma7/data/durham/dc-grov1/Halo_mass_pair_binning/BGS/haloes_z0.2.hdf5"
+
+boxsize = 2000
+
+r_bin_edges = np.logspace(-2,2.2,85)
+
+mass_bin_edges = np.logspace(1,6,101)
+
+num_sat_parts = 3
+
+run_label = "test_case"
+
+
