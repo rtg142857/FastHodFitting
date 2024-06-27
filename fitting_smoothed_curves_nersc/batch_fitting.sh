62c80bd49d561b683f536bde6fc9a23138a2d322
Alexander M <rtg142857@gmail.com>
2024-06-27 15:41:27 +0100

    Redoing batch script to fit cosma

--- fitting_smoothed_curves_nersc/batch_fitting.sh
+++ fitting_smoothed_curves_nersc/batch_fitting.sh
@@ -2,17 +2,22 @@
 
 #!/bin/bash
 #SBATCH -q regular
-#SBATCH -o log_fitting
+#SBATCH -o logs/fitting
+#SBATCH -e logs/fitting_error
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH -C cpu
-#SBATCH --mail-user=alexander.m.smith@durham.ac.uk 
+#SBATCH --mail-user=tlrt88@durham.ac.uk 
 
-cosmodesienv main
+module purge
+module use /cosma/home/dp004/dc-mene1/software/desi/cosmodesiconda/my-desiconda/modulefiles
+module load cosmodesiconda/my-desiconda
+#cosmodesienv main
 
-module load gcc
+#module load gcc
 module load gsl
-module unload craype-hugepages2M
+#module unload craype-hugepages2M
+module load python/3.10.12
 
-python3 fasthod_fitting.py 1
+python fasthod_fitting.py 1
