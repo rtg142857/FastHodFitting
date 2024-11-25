4cce0aee92a5d9b69728a74be306f94d8b320ca6
Alexander M. <rtg142857@gmail.com>
2024-11-25 13:48:19 +0000

    Debugging

--- fitting_smoothed_curves_nersc/plot_HODs_smooth.py
+++ fitting_smoothed_curves_nersc/plot_HODs_smooth.py
@@ -29,10 +29,14 @@ def plot_HODs(HODs):
 
 
 
-def get_HODs(params):
-    hod_cen_big = Cen_HOD(params,mass_bin_centres)
-    hod_sat_big = Sat_HOD(params,hod_cen_big,mass_bin_centres)
-    return hod_cen_big + hod_sat_big
+def get_HODs(HOD_params):
+    HODs = np.zeros((len(mass_bin_edges)-1,11))
+    for i in range(11):
+        params = HOD_params[i,:]
+        hod_cen = Cen_HOD(params,mass_bin_centres)
+        hod_sat = Sat_HOD(params,hod_cen,mass_bin_centres)
+        HODs[:,i] = hod_cen + hod_sat
+    return HODs
 
 
 HOD_params = np.genfromtxt(save_path+"_best_params.txt")
